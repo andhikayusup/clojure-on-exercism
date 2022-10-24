017243fb95de51773c17481174a33b3379859141
andhikayusup <andeeka.ym@gmail.com>
2022-10-24 15:15:43 +0700

    build: initial commit

---
+++ test/clojure_on_exercism/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-on-exercism.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-on-exercism.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
